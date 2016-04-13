6664ccacf16fece5a005655081fcab7405c868eb
puneet <pg1815@gmail.com>
2016-04-14 01:38:26 +0530

    polls app

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,55 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from .models import Question
 from django.conf.urls import url
+from django.views import generic
 from . import views
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 # Create your views here.
-
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
-	
-def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
-	 
-def results(request,question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % question_id)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
-def vote(request,question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 	
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+	
+	
+app_name = 'polls'
 urlpatterns = [
-	url(r'^$', view.index, name='index'),
-	url(r'(^?P<question_id>[0-9]+)/results/$', view.results, name='results'),
-	url(r'(^P<question_id>[0-9]+/vote/$)',view.vote, name='vote'),
-]	
+    url(r'^$', views.index, name='index'),
+]
